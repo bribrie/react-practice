a89fc3da45bdee0bf71641baa113a544568f9569
bribrie <ibriebriee@gmail.com>
2022-01-14 23:36:51 +0900

    Fetch data using useEffect

--- fetch-data/src/App.js
+++ fetch-data/src/App.js
@@ -1,5 +1,59 @@
-function App() {
-  return <div></div>;
-}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default App;
+export default function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("redux");
+  const [url, setUrl] = useState(
+    `https://hn.algolia.com/api/v1/search?query=redux`
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`);
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {isError && <div>=Something went wrong...</div>}
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {data.hits.map((item) => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
