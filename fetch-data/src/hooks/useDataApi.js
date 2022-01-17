c6ad53d59fa6f07b95d6303d443d67aca8b9658d
bribrie <ibriebriee@gmail.com>
2022-01-17 20:21:47 +0900

    Combine three states using useReducer

--- fetch-data/src/hooks/useDataApi.js
+++ fetch-data/src/hooks/useDataApi.js
@@ -1,26 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import axios from "axios";
 
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return { ...state, isLoading: true, isError: false };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "FETCH_FAILURE":
+      return { ...state, isLoading: false, isError: true };
+    default:
+      throw new Error();
+  }
+};
+
 export default function useDataApi(initialUrl, initialData) {
-  const [data, setData] = useState(initialData);
   const [url, setUrl] = useState(initialUrl);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
+      dispatch({ type: "FETCH_INIT" });
       try {
         const result = await axios(url);
-        setData(result.data);
-      } catch {
-        setIsError(true);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAILURE" });
       }
-      setIsLoading(false);
     };
 
     fetchData();
   }, [url]);
-  return [{ data, isLoading, isError }, setUrl];
+
+  return [state, setUrl];
 }
