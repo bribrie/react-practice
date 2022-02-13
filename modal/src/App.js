d0ac74bfef4bdc08f4e1d673fc6931799eab2346
bribrie <ibriebriee@gmail.com>
2022-02-13 23:38:08 +0900

    Add User list

--- modal/src/App.js
+++ modal/src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const onAddUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <>
-      <AddUser />
+      <AddUser onAddUser={onAddUserHandler} />
+      <UserList users={userList} />
     </>
   );
 }
