d21c03ac1798f2e73791cd375ad5753070ab5302
bribrie <ibriebriee@gmail.com>
2022-02-16 23:24:29 +0900

    Modal) Add Portal

--- modal/src/components/UI/ErrorModal.js
+++ modal/src/components/UI/ErrorModal.js
@@ -2,26 +2,45 @@ import React from "react";
 import Card from "./Card";
 import Button from "./Button";
 import styled from "./ErrorModal.module.css";
+import { createPortal } from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styled.backdrop} onClick={props.onConfrim}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styled.modal}>
+      <header className={styled.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styled.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styled.action}>
+        <Button onClick={props.onConfrim}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = (props) => {
-  const { title, message, onConfrim } = props;
-
   return (
     <>
-      <div className={styled.backdrop} onClick={onConfrim}>
-        <Card className={styled.modal}>
-          <header className={styled.header}>
-            <h2>{title}</h2>
-          </header>
-          <div className={styled.content}>
-            <p>{message}</p>
-          </div>
-          <footer className={styled.action}>
-            <Button onClick={props.onConfrim}>Okay</Button>
-          </footer>
-        </Card>
-      </div>
+      {createPortal(
+        <Backdrop onConfrim={props.onConfrim} />,
+        document.getElementById("backdrop-root")
+      )}
+      {createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfrim={props.onConfrim}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
+
 export default ErrorModal;
